905b254ebdee9935417549c3ef289563ceb4bf52
Emile LEPETIT <emile.lepetit@gmail.com>
2020-01-27 16:29:46 +0100

    Add tests for Ex00 & Ex01

---
+++ ex00_tests/picture_tests.cpp
@@ -0,0 +1,65 @@
+#include <criterion/criterion.h>
+
+#include <string>
+#include <ostream>
+#include <iostream>
+
+#include "Picture.hpp"
+
+class OSRedirector {
+    private:
+        std::ostringstream _oss;
+        std::streambuf *_backup;
+        std::ostream &_c;
+
+
+    public:
+        OSRedirector(std::ostream &c) : _c(c) {
+            _backup = _c.rdbuf();
+            _c.rdbuf(_oss.rdbuf());
+        }
+
+        ~OSRedirector() {
+            _c.rdbuf(_backup);
+        }
+
+        const std::string getContent() {
+            _oss << std::flush;
+            return _oss.str();
+        }
+};
+
+const std::string image = "         _|_\n"
+              "   ,_.-_' _ '_-._,\n"
+              "    \\ (.)(.)(.) /\n"
+              " _,  `\\_-===-_/`  ,_\n"
+              ">  |----\"\"\"\"\"----|  <\n"
+              "`\"\"`--/   _-@-\\--`\"\"`\n"
+              "     |===L_I===|\n"
+              "      \\       /\n"
+              "      _\\__|__/_\n"
+              "     `\"\"\"\"`\"\"\"\"`\n";
+
+Test(Picture, new_empty) {
+    Picture* pic = new Picture();
+    cr_assert(pic->data == "");
+    delete pic;
+}
+
+Test(Picture, new_from_file) {
+    Picture* pic = new Picture("../media/alien.txt");
+    cr_assert(pic->data == image);
+
+    Picture* wrong = new Picture("ezuiehuizheu");
+    cr_assert(wrong->data == "ERROR");
+}
+
+Test(Picture, getFromFile) {
+    Picture* pic = new Picture();
+    bool status = pic->getPictureFromFile("../media/alien.txt");
+    cr_assert(pic->data == image);
+    cr_assert(status == true);
+    status = pic->getPictureFromFile("kjeioioeji");
+    cr_assert(pic->data == "ERROR");
+    cr_assert(status == false);
+}
--- run_tests.sh
+++ run_tests.sh
@@ -16,6 +16,20 @@ fi
 cd ..
 echo "--- End testing ex00 ---"
 
+echo "--- Testing ex01 ---"
+cp ../ex01/* ex01_tests
+cd ex01_tests
+g++ -std=c++14 -Wall -Wextra -fprofile-arcs -ftest-coverage --coverage -lcriterion *.cpp -o ex01
+if [ $? -ne 0 ]; then
+    ((err++))
+fi
+./ex00 --ascii
+if [ $? -ne 0 ]; then
+    ((err++))
+fi
+cd ..
+echo "--- End testing ex00 ---"
+
 if [ $err -ne 0 ]; then
     exit 1
 fi
